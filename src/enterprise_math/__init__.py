335e4c41c0a30aa0355f2f2f624d71a73b1b3523
awdawmip <30957095+awdawmip@users.noreply.github.com>
2026-08-08 21:15:58 +0800

    Export intrinsic geometry helpers

--- src/enterprise_math/__init__.py
+++ src/enterprise_math/__init__.py
@@ -1,3 +1,15 @@
 from .core import basin_for_root, collapse, integer_nth_root, preimage_count, project_scale, scaled_root
+from .geometry import graph_distance, l1_distance, lattice2_ball, lattice2_sphere
 
-__all__ = ["integer_nth_root", "collapse", "basin_for_root", "preimage_count", "scaled_root", "project_scale"]
+__all__ = [
+    "integer_nth_root",
+    "collapse",
+    "basin_for_root",
+    "preimage_count",
+    "scaled_root",
+    "project_scale",
+    "graph_distance",
+    "l1_distance",
+    "lattice2_sphere",
+    "lattice2_ball",
+]
