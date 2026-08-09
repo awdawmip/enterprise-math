c8fa091e71ae892d47869c124b00fc4e659fda7f
awdawmip <30957095+awdawmip@users.noreply.github.com>
2026-08-09 08:23:08 +0800

    P019: test local tree-rotation imbalance transport

--- tests/test_pair_dispersion.py
+++ tests/test_pair_dispersion.py
@@ -5,8 +5,11 @@ from enterprise_math.pair_dispersion import (
     merge_pair_dispersion_identity,
     pair_dispersion,
     pair_dispersion_identity,
+    reassociate_imbalances,
+    reassociation_quadratic_identity,
     zero_sum_quadratic_separation,
 )
+from enterprise_math.contraction_trace import square_split_imbalance
 
 
 class PairDispersionTests(unittest.TestCase):
@@ -26,6 +29,71 @@ class PairDispersionTests(unittest.TestCase):
                             left, right
                         )
                         self.assertEqual(left_side, right_side)
+
+    def test_local_reassociation_transports_imbalances_exactly(self):
+        for left_size in range(1, 4):
+            for middle_size in range(1, 4):
+                for right_size in range(1, 4):
+                    for left_total in range(-4, 5):
+                        for middle_total in range(-4, 5):
+                            for right_total in range(-4, 5):
+                                old_inner = square_split_imbalance(
+                                    left_size,
+                                    middle_size,
+                                    left_total,
+                                    middle_total,
+                                )
+                                old_parent = square_split_imbalance(
+                                    left_size + middle_size,
+                                    right_size,
+                                    left_total + middle_total,
+                                    right_total,
+                                )
+                                new_inner, new_parent = reassociate_imbalances(
+                                    left_size,
+                                    middle_size,
+                                    right_size,
+                                    old_inner,
+                                    old_parent,
+                                )
+                                expected_inner = square_split_imbalance(
+                                    middle_size,
+                                    right_size,
+                                    middle_total,
+                                    right_total,
+                                )
+                                expected_parent = square_split_imbalance(
+                                    left_size,
+                                    middle_size + right_size,
+                                    left_total,
+                                    middle_total + right_total,
+                                )
+                                self.assertEqual(
+                                    (new_inner, new_parent),
+                                    (expected_inner, expected_parent),
+                                )
+                                identity_left, identity_right = (
+                                    reassociation_quadratic_identity(
+                                        left_size,
+                                        middle_size,
+                                        right_size,
+                                        old_inner,
+                                        old_parent,
+                                    )
+                                )
+                                self.assertEqual(identity_left, identity_right)
+
+    def test_unit_slot_rotation_matches_closed_example(self):
+        # ((x,y),z) -> (x,(y,z))
+        for x in range(-4, 5):
+            for y in range(-4, 5):
+                for z in range(-4, 5):
+                    u = x - y
+                    v = x + y - 2 * z
+                    up, vp = reassociate_imbalances(1, 1, 1, u, v)
+                    self.assertEqual(2 * up, v - u)
+                    self.assertEqual(2 * vp, v + 3 * u)
+                    self.assertEqual(3 * u * u + v * v, 3 * up * up + vp * vp)
 
     def test_zero_sum_pair_dispersion_recovers_quadratic_separation(self):
         for coordinate_count in range(2, 7):
