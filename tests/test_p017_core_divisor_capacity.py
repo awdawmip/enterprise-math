955e63e1faed192f53cca176ce153849df24fed2
awdawmip <30957095+awdawmip@users.noreply.github.com>
2026-08-10 09:03:36 +0800

    Test exact aligned divisor capacity

--- tests/test_p017_core_divisor_capacity.py
+++ tests/test_p017_core_divisor_capacity.py
@@ -21,7 +21,23 @@ class P017CoreDivisorCapacityTests(unittest.TestCase):
         capacity = signed_divisor_capacity(31, 15)
         self.assertEqual(capacity["raw_signed_points"], (-13, 17))
         self.assertEqual(capacity["anchor_signed_points"], (-13, 17))
+        self.assertEqual(capacity["exact_aligned_capacity"], 2)
+        self.assertEqual(capacity["exact_anchor_capacity"], 2)
         self.assertEqual(capacity["universal_capacity"], 3)
+
+    def test_alignment_can_strictly_improve_universal_capacity(self):
+        data = signed_divisor_capacity(20_000, 17_017)
+        self.assertEqual(data["exact_aligned_capacity"], 1)
+        self.assertEqual(data["exact_anchor_capacity"], 1)
+        self.assertEqual(data["universal_capacity"], 2)
+        self.assertEqual(data["anchor_signed_points"], (1_381,))
+
+    def test_anchor_filter_can_strictly_improve_aligned_capacity(self):
+        data = signed_divisor_capacity(20_000, 19_019)
+        self.assertEqual(data["raw_signed_points"], (-6_627,))
+        self.assertEqual(data["exact_aligned_capacity"], 1)
+        self.assertEqual(data["exact_anchor_capacity"], 0)
+        self.assertEqual(data["universal_capacity"], 2)
 
     def test_divisor_above_radius_range_is_globally_single_use(self):
         data = signed_divisor_capacity(31, 35)
@@ -32,12 +48,14 @@ class P017CoreDivisorCapacityTests(unittest.TestCase):
 
     def test_prime_power_is_included_as_special_case(self):
         data = signed_divisor_capacity(64, 27)
-        self.assertLessEqual(data["anchor_count"], (64 - 1) // 27 + 1)
+        self.assertLessEqual(data["exact_anchor_capacity"], data["exact_aligned_capacity"])
+        self.assertLessEqual(data["exact_aligned_capacity"], (64 - 1) // 27 + 1)
         self.assertEqual(data["signed_modulus"], 54)
 
-    def test_selected_incidence_family_obeys_same_capacity(self):
+    def test_selected_incidence_family_obeys_exact_anchor_capacity(self):
         data = selected_signed_divisor_incidence_capacity(31, 15, (-13, 17))
         self.assertEqual(data["selected_count"], 2)
+        self.assertEqual(data["exact_anchor_capacity"], 2)
         self.assertEqual(data["universal_capacity"], 3)
 
     def test_anchor_filter_only_removes_raw_points(self):
