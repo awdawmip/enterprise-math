e09ed49dcfb38d47ebf42de10d2e0c68c95393a3
awdawmip <30957095+awdawmip@users.noreply.github.com>
2026-08-25 10:54:13 +0800

    [EM-PFF1-6DA3FD] add Prime Fusion F1 root aggregator

---
+++ EnterpriseMath/PrimeFusion.lean
@@ -0,0 +1,7 @@
+import EnterpriseMath.PrimeFusion.Channels
+import EnterpriseMath.PrimeFusion.FusionAlgebra
+import EnterpriseMath.PrimeFusion.ArithmeticSplit
+import EnterpriseMath.PrimeFusion.MixedLocus
+import EnterpriseMath.PrimeFusion.PhaseReadout
+import EnterpriseMath.PrimeFusion.PointedQuotient
+import EnterpriseMath.PrimeFusion.PointedRecovery
