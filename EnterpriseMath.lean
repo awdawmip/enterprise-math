c668f02d50276b5d9b366ee1338f447cf590afed
awdawmip <30957095+awdawmip@users.noreply.github.com>
2026-09-05 14:33:08 +0800

    Import generic BRC observer algebra layer

--- EnterpriseMath.lean
+++ EnterpriseMath.lean
@@ -16,6 +16,7 @@ import EnterpriseMath.Quotient.RootFutureClosure
 import EnterpriseMath.Relation.BranchRecoalescence
 import EnterpriseMath.Relation.FramedBranchRecoalescence
 import EnterpriseMath.Relation.BRCFrameSymmetry
+import EnterpriseMath.Relation.BRCObserverAlgebra
 import EnterpriseMath.Relation.BRCWeightHistogram
 import EnterpriseMath.Relation.BRCCountAtlas
 import EnterpriseMath.Scale.Compatibility
