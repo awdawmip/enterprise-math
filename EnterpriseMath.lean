3371098b4b45dd6536e079b07adc0f89ef411bb0
awdawmip <30957095+awdawmip@users.noreply.github.com>
2026-09-07 21:23:56 +0800

    [EM-BRCWLOG-6F42A1] Add atlas polynomial emission to Lean umbrella

--- EnterpriseMath.lean
+++ EnterpriseMath.lean
@@ -24,6 +24,7 @@ import EnterpriseMath.Relation.BRCCountAtlas
 import EnterpriseMath.Relation.BRCSixAxisS4
 import EnterpriseMath.Relation.BRCK4OptimalExtraction
 import EnterpriseMath.Relation.BRCFiniteFrameLift
+import EnterpriseMath.Relation.BRCAtlasPolynomialEmission
 import EnterpriseMath.Scale.Compatibility
 import EnterpriseMath.State.CriticalGrid
 import EnterpriseMath.PrimeFusion
