13c3f85509a8cd3c72ebb39fdf24e82a7cb2e180
awdawmip <30957095+awdawmip@users.noreply.github.com>
2026-09-07 22:06:18 +0800

    [EM-BRCWLOG-6F42A1] Add K4 parity-hole algebra to Lean umbrella

--- EnterpriseMath.lean
+++ EnterpriseMath.lean
@@ -24,6 +24,7 @@ import EnterpriseMath.Relation.BRCCountAtlas
 import EnterpriseMath.Relation.BRCSixAxisS4
 import EnterpriseMath.Relation.BRCK4OptimalExtraction
 import EnterpriseMath.Relation.BRCK4FactorizationAlgebra
+import EnterpriseMath.Relation.BRCK4ParityHole
 import EnterpriseMath.Relation.BRCFiniteFrameLift
 import EnterpriseMath.Relation.BRCAtlasPolynomialEmission
 import EnterpriseMath.Relation.BRCAtlasCarryAlgebra
